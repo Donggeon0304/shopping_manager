8c004611abc06b2e45109c00023a18f37e92b7fd
Donggeon0304 <rodzld18@naver.com>
2024-07-26 11:47:27 +0900

    web_controller

--- shopping_mall/src/main/java/com/coupangmall/www/web_controller.java
+++ shopping_mall/src/main/java/com/coupangmall/www/web_controller.java
@@ -1,9 +1,15 @@
 package com.coupangmall.www;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class web_controller {
 	
+	@GetMapping("/ajaxok.do")
+	public String ajaxok() {
+		
+		return null;
+	}
 	
 }

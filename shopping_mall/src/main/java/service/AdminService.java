91276edc2be101165c905121a827d4e2894932d5
Donggeon0304 <rodzld18@naver.com>
2024-08-05 09:39:20 +0900

    test

--- shopping_mall/src/main/java/service/AdminService.java
+++ shopping_mall/src/main/java/service/AdminService.java
@@ -2,8 +2,6 @@ package service;
 
 import java.util.List;
 
-import javax.annotation.Resource;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
